834d95b70a8223bec050cc026b89378a26546bda
Elandland <bodan99@gmail.com>
2024-06-12 11:05:52 +0900

    Feat. 백테스팅 API작성

--- stock_backtest.py
+++ stock_backtest.py
@@ -1,61 +1,33 @@
-# %%
-'''
-https://j-smallworld.tistory.com/15
-리밸런싱 백테스트
-'''
+from flask import Flask, request, jsonify
 import requests
-import json 
-import pandas as pd 
+import pandas as pd
 from bs4 import BeautifulSoup
 import re
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 import numpy as np
 
-import matplotlib as rc
-rc.use('TkAgg')
-import matplotlib.pyplot as plt
-import platform
-if platform.system() == 'Darwin': #맥
-    plt.rcParams['font.family'] ='AppleGothic'
-elif platform.system() == 'Windows': #윈도우
-    plt.rcParams['font.family'] = 'Malgun Gothic'
-elif platform.system() == 'Linux': #리눅스
-    plt.rcParams['font.family'] = 'Malgun Gothic' 
-plt.rcParams['axes.unicode_minus'] = False #한글 폰트 사용시 마이너스 폰트 깨짐 해결
+app = Flask(__name__)
 
+@app.route('/backtest', methods=['POST'])
+def backtest():
+    data = request.json
+    result = back_test(data)
+    return jsonify(result)
 
-# 종목코드별 상장일
-def get_stock_origintime(code): 
-    """ 
-    Naver 금융 데이터 페이지를 크롤링하여 특정 종목의 상장일 획득
-    :param code: 종목 코드 
-    :return: 종목코드 시작일자(origintime) 
-    """ 
-    
+def get_stock_origintime(code):
     stock_data = []
-    url = "https://fchart.stock.naver.com/sise.nhn?symbol={}&timeframe=day&count=1&requestType=0".format(code) 
-    html = requests.get(url).text 
-    soup = BeautifulSoup(html, "xml") 
-    origintime = soup.select_one("chartdata")['origintime'] 
-     
+    url = "https://fchart.stock.naver.com/sise.nhn?symbol={}&timeframe=day&count=1&requestType=0".format(code)
+    html = requests.get(url).text
+    soup = BeautifulSoup(html, "xml")
+    origintime = soup.select_one("chartdata")['origintime']
     return origintime
 
-
-# 주가 데이터 조회
 def get_stock_data(code, from_date, to_date):
-    """
-    Naver 금융 데이터 페이지를 크롤링하여 주가 기록 조회
-    :param code: 종목 코드
-    :param from_date: 조회 시작일자
-    :param to_date: 조회 종료일자
-    :return: 해당 종목 코드의 일자, 시가, 고가, 저가, 종가, 거래량 데이터프레임
-    """
-
     from_date = str(from_date)
     to_date = str(to_date)
     count = (datetime.today() - datetime.strptime(from_date, "%Y%m%d")).days + 1
-    
+
     stock_data = []
     url = "https://fchart.stock.naver.com/sise.nhn?symbol={}&timeframe=day&count={}&requestType=0".format(code, count)
     html = requests.get(url).text
@@ -76,14 +48,9 @@ def get_stock_data(code, from_date, to_date):
     df.set_index(keys='date', inplace=True)
     return df
 
-
-# 리밸런싱 하는 경우
 def buy_stock(money, stock_price, last_stock_num, stock_rate):
-    '''
-    총 평가금액을 기준으로 설정 비율대로 리밸런싱 수행
-    '''
     if stock_price == 0:
-        return money, 0, 0  
+        return money, 0, 0
 
     stock_num = money * stock_rate // stock_price
     stock_money = stock_num * stock_price
@@ -100,14 +67,7 @@ def buy_stock(money, stock_price, last_stock_num, stock_rate):
     money -= (stock_money + buy_sell_fee)
     return money, stock_num, stock_money
 
-
-# 월 적립만 하는 경우
 def buy_stock_more(money, stock_price, last_stock_num, stock_rate):
-    '''
-    StockInfo.json 에서 interval_month 을 0으로 설정하는 경우
-    기존 구매한 종목에 대해서는 리밸런싱 하지 않고
-    추가 투자 금액만 가지고 비율대로 매수만 수행
-    '''
     if stock_price == 0:
         return money, 0, 0
 
@@ -124,23 +84,12 @@ def buy_stock_more(money, stock_price, last_stock_num, stock_rate):
         buy_sell_fee = stock_num * stock_price * fee
     money -= (stock_money + buy_sell_fee)
 
-    # 추가 매수한 만큼 더해준다
     stock_num = stock_num + last_stock_num
     stock_money = stock_num * stock_price
 
     return money, stock_num, stock_money
 
-
 def get_ratio(names, prices, ratios):
-    '''
-    상장일이 모두 다르기 때문에 특정 기간 동안에는 일부 종목이 매수되지 않게 된다.
-    이때는 상장 되어있는 종목으로만 구성하여 자산 비율을 재계산 한다.
-    예) 4개의 종목을 각각 25%씩 구성 -> 백테스팅 기간 중 앞에 1년은 두 개 종목만 상장되어 있는 상태.
-       상장된 두 개의 종목 구성 비율을 50%로 설정하여 현금을 최대한 투자에 사용하도록 함.
-    
-    현금이 남더라도 기존 설정 비율대로만 매수하도록 테스트 할 경우, 이 함수 맨 마지막 한 줄을 다음과 같이 변경하여 사용
-    return new_ratios --> return ratios
-    '''
     total_ratio = 0
     new_ratios = []
     for name in names:
@@ -156,39 +105,24 @@ def get_ratio(names, prices, ratios):
     return new_ratios
 
 def get_month_end_data(df):
-    df.index = pd.to_datetime(df.index)  # 인덱스를 DatetimeIndex로 변환
+    df.index = pd.to_datetime(df.index)
     return df.resample('ME').last()
 
-def calculate_sharpe_ratio_and_std(df, risk_free_rate=0.01):
-    df.index = pd.to_datetime(df.index)  # 인덱스를 DatetimeIndex로 변환
-    df['monthly_return'] = df['backtest'].pct_change().dropna()  # 월간 수익률 계산 후 결측치 제거
-    
-    # 월간 수익률의 평균 계산
-    mean_monthly_return = df['monthly_return'].mean()
-    
-    # 월간 수익률의 표준편차 계산
+def calculate_sharpe_ratio_and_std(df, risk_free_rate=0.03):
+    df.index = pd.to_datetime(df.index)
+    df['monthly_return'] = df['backtest'].pct_change().dropna()
+
     monthly_std_dev = df['monthly_return'].std()
-    
-    # 누적 수익률 계산
+
     cumulative_return = df['backtest'].iloc[-1] / df['backtest'].iloc[0] - 1
-    
-    # 연간 수익률 계산
     total_period_years = (df.index[-1] - df.index[0]).days / 365.25
     annual_return = (1 + cumulative_return) ** (1 / total_period_years) - 1
-    
-    # 연간 표준편차로 변환
     annual_std_dev = monthly_std_dev * np.sqrt(12)
-    
-    # 샤프 비율 계산
     sharpe_ratio = (annual_return - risk_free_rate) / annual_std_dev
-    
 
-    
     return round(sharpe_ratio, 2), round(annual_std_dev * 100, 2), round(annual_return, 2)
 
-
 def back_test_portfolio(money: int, interval: int, start_day: str, end_day: str, stock_list, start_from_latest_stock: str):
-
     total_invest_money = money
 
     stock_code = []
@@ -214,49 +148,42 @@ def back_test_portfolio(money: int, interval: int, start_day: str, end_day: str,
             if first_date == 0 or first_date > org_time:
                 first_date = org_time
 
-    # 백테스팅 시작 날짜가 주식 리스트 중 가장 첫 상장일보다 빠른 경우 보정
     if first_date > start_day:
         start_day = first_date
-    
-    start_date = datetime.strptime(start_day, '%Y%m%d')  # 조회시작일
 
+    start_date = datetime.strptime(start_day, '%Y%m%d')
     cal_days = (datetime.strptime(end_day, "%Y%m%d") - start_date).days
-    
-    df = pd.DataFrame() 
 
-    for i in range(len(stock_code)): 
+    df = pd.DataFrame()
+
+    for i in range(len(stock_code)):
         df_close = get_stock_data(stock_code[i], start_day, end_day)['close']
-        df_close = df_close.rename(stock_name[i])  # 열 이름을 종목 이름으로 변경
-        df_close.index = pd.to_datetime(df_close.index)  # 인덱스를 DatetimeIndex로 변환
-        df_close = get_month_end_data(df_close)  # 월말 데이터만 추출
+        df_close = df_close.rename(stock_name[i])
+        df_close.index = pd.to_datetime(df_close.index)
+        df_close = get_month_end_data(df_close)
         df = pd.merge(df, df_close, how='outer', left_index=True, right_index=True)
-
 
     df.columns = stock_name
     df.fillna(0, inplace=True)
 
-    # 모든 주식이 상장된 이후의 날짜를 기준으로 필터링
     if start_from_latest_stock == "true":
         latest_start_date = max(pd.to_datetime([get_stock_origintime(code) for code in stock_code]))
         df = df[df.index >= latest_start_date]
 
-    # 리밸런싱 날짜 리스트 저장
     rebalanceing_date_list = []
     while start_date <= df.index[-1]:
         temp_date = start_date
         while temp_date not in df.index and temp_date < df.index[-1]:
-            temp_date += timedelta(days=1)  # 영업일이 아닐 경우 1일씩 증가.
+            temp_date += timedelta(days=1)
         rebalanceing_date_list.append(temp_date)
-        start_date += relativedelta(months=interval)  # interval 개월씩 증가.
-
+        start_date += relativedelta(months=interval)
 
     backtest_index = []
     backtest_data = []
 
-    etf_num = {etf: 0 for etf in stock_name} # 구매한 ETF 개수
-    prices = {etf: 0 for etf in stock_name} # 현재가
-    etf_money = {etf: 0 for etf in stock_name} # 평가금액
-
+    etf_num = {etf: 0 for etf in stock_name}
+    prices = {etf: 0 for etf in stock_name}
+    etf_money = {etf: 0 for etf in stock_name}
 
     date_idx = 0
     for each in df.index:
@@ -268,7 +195,6 @@ def back_test_portfolio(money: int, interval: int, start_day: str, end_day: str,
 
         for stock in stock_name:
             prices[stock] = df[stock][each]
-            # 리밸런싱 하는 달에는 총 금액 대비 비율 계산을 위해 보유 종목 매도 처리
             if rebalnace_day is True:
                 money += etf_num[stock] * prices[stock]
 
@@ -278,32 +204,26 @@ def back_test_portfolio(money: int, interval: int, start_day: str, end_day: str,
         cal = 0
 
         for stock in stock_name:
-            # 종목 매입
             try:
                 if rebalnace_day is True:
-                    # 리밸런싱 하는 경우, 전체 금액을 기준으로 투자 비율대로 매수
                     money, etf_num[stock], etf_money[stock] = buy_stock(money, prices[stock], etf_num[stock], recal_ratio[stock_name.index(stock)]/((1-cal) if cal < 1 else 1))
                 else:
-                    # 추가 매수만 하는 경우, 월 적립금을 기준으로 투자 비율대로 나누어 매수
                     money, etf_num[stock], etf_money[stock] = buy_stock_more(money, prices[stock], etf_num[stock], recal_ratio[stock_name.index(stock)]/((1-cal) if cal < 1 else 1))
             except Exception as e:
                 print(e)
 
             if etf_num[stock] > 0:
-                total += etf_money[stock] 
+                total += etf_money[stock]
                 cal += recal_ratio[stock_name.index(stock)]
 
         total += money
         backtest_index.append(each)
         backtest_data.append(int(total)/total_invest_money)
 
-
     backtest_df = pd.DataFrame(backtest_data, index=backtest_index, columns=['backtest'])
 
-    # 최종 데이터 프레임, 3개의 지표와 백테스트 결과
     final_df = pd.concat([df, backtest_df], axis=1)
 
-    # 시작점을 1로 통일함.
     for stock in stock_name:
         for pr in final_df[stock]:
             if pr > 0:
@@ -313,12 +233,11 @@ def back_test_portfolio(money: int, interval: int, start_day: str, end_day: str,
     final_df.index = final_df.index.astype(str)
     final_df_dict = final_df.to_dict()
 
-    # 샤프 비율, 표준편차, 연간 수익률 계산
     sharpe_ratio, annual_std_dev, annual_return = calculate_sharpe_ratio_and_std(final_df)
 
     return final_df, final_df_dict, sharpe_ratio, annual_std_dev, annual_return, total
 
-def back_test(stock_info):  
+def back_test(stock_info):
     portfolio = stock_info['portfolio']
     start_from_latest_stock = stock_info['start_from_latest_stock']
 
@@ -328,61 +247,11 @@ def back_test(stock_info):
     start_date = portfolio['start_date']
     end_date = portfolio['end_date']
 
-    # back_test_portfolio 호출 시 인자가 누락된 오류 수정
     final_df, final_df_dict, sharpe_ratio, annual_std_dev, annual_return, total_balance = back_test_portfolio(balance, interval, start_date, end_date, stock_list, start_from_latest_stock)
-    
+
     result = {'portfolio': final_df_dict, 'sharpe_ratio': sharpe_ratio, 'standard_deviation': annual_std_dev, 'annual_return': annual_return, 'total_balance': total_balance}
-    
-    bbox = dict( 
-        boxstyle='square',
-        facecolor='white',
-    )
-
-    plt.rcParams["figure.figsize"] = (8, 4)
-    plt.subplots(constrained_layout=True)
-
-    plt.title("Portfolio (rebalancing interval(month):" + str(interval) + ")")
-
-    height = final_df['backtest'].iloc[-1]
-    plt.plot(final_df['backtest'].index, final_df['backtest'], label='Backtest (%.2f)' % height)      
-
-    stock_name = []
-    for sss in stock_list:
-        stock_name.append(sss[1])
-
-    for stock in stock_name:
-        height = final_df[stock].iloc[-1]
-        plt.plot(final_df[stock].index, final_df[stock], label=stock + "(%.2f)" % height, linestyle='--', alpha=0.3)
-
-    plt.ylabel("수익률(최종평가액/투자원금)")
-    plt.xlabel("날짜")
-    plt.legend(loc='upper left')
-    plt.grid(True)
-
-    plt.savefig("stock_backtest.png")
-
-    # 결과를 JSON 파일로 저장 (덮어쓰기)
-    with open('result.json', 'w', encoding='utf-8') as f:
-        json.dump(result, f, ensure_ascii=False, indent=4)
 
     return result
 
-# 클라이언트로부터 JSON 데이터를 받았다고 가정하고 실행
-client_json_data = {
-    "start_from_latest_stock": "false",     #이게 true면 가장 늦게 상장된 걸 기준으로 백테스팅 ,false면 가장 먼저 상장된걸 기준으로 백테스팅
-    "portfolio": {
-        "stock_list": [
-            ["360750", "TIGER 나스닥100", 0.25],
-            ["133690", "TIGER 미국나스닥100", 0.25],
-            ["309230", "KINDEX 미국WideMoat가치주", 0.25],
-            ["381180", "TIGER 미국필라델피아반도체나스닥", 0.25]
-        ],
-        "balance": 1000000,
-        "interval_month": 1,        #리밸런싱할 기간. 1달마다 다시 0.25퍼가 되도록 매수 매도를 진행
-        "start_date": "20100101",
-        "end_date": "20221231"
-    }
-}
-
-result = back_test(client_json_data)
-# %%
+if __name__ == '__main__':
+    app.run(debug=True)
